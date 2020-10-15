2af6ec1c42cb3a1d49379226b1fb268e71aac2f2
John <johncraigmurphy@gmail.com>
2020-10-15 16:08:56 +0100

    save

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -49,16 +49,59 @@ class Controller extends BaseController
                 $constituenciesLink = "http://lda.data.parliament.uk/electionresults.xml?_pageSize=650&electionId=" . $electionResourceNumber . "&_page=0";
 
 
-                dd($constituenciesLink);
+                // dd($constituenciesLink);
 
-                return $decode_response["primaryTopic"];
+                // return $decode_response["primaryTopic"];
     
-            default: // Response json
-                $encode_response = json_encode($response);   
+            // default: // Response json
+            //     $encode_response = json_encode($response);   
     
-                $decode_response = json_decode($encode_response, TRUE);
-                return json_decode($decode_response, TRUE);   
+            //     $decode_response = json_decode($encode_response, TRUE);
+            //     return json_decode($decode_response, TRUE);   
             }
+
+
+            //constituencies
+
+            // dd($constituenciesLink);
+
+            $response = $client->get($constituenciesLink, $param_data);
+            $response = $response->getBody()->getContents();
+    
+            switch ($provider_type) {
+                case 'application/xml':
+                    $encode_response = json_encode(simplexml_load_string($response));   
+        
+                    $decode_response = json_decode($encode_response, TRUE);
+                    // dd($decode_response["@attributes"]);
+                    // $electionName = $decode_response;
+                    // $data = $decode_response["primaryTopic"]["@attributes"]["href"];
+    
+                    //get the election number from the link and create a new link for constituencies
+                    // $electionResourceNumber = substr($data, strpos($data, "resources/") + 10); 
+    
+                    // $constituenciesLink = "http://lda.data.parliament.uk/electionresults.xml?_pageSize=650&electionId=" . $electionResourceNumber . "&_page=0";
+    
+    
+                    // dd($decode_response);
+    
+                    // return $decode_response["items"]["item"];
+
+                    foreach ($decode_response["items"]["item"] as $constituency){
+
+                        $constituencyName = $constituency["constituency"]["label"];
+                        $constituencyCandidatesLink["constituency"]["@attributes"]["href"];
+
+                        dd($constituency);
+
+                    }
+        
+                // default: // Response json
+                //     $encode_response = json_encode($response);   
+        
+                //     $decode_response = json_decode($encode_response, TRUE);
+                //     return json_decode($decode_response, TRUE);   
+                }
         
         return $response;
         // return view('products/show',compact('response'));
